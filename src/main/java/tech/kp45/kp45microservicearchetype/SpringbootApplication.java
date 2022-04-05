4af54c0871b2258398098342bbae8c2073bea45a
KP45 <kevinpan45@163.com>
2022-04-05 20:31:07 +0800

    fix bug

--- src/main/java/tech/kp45/kp45microservicearchetype/SpringbootApplication.java
+++ src/main/java/tech/kp45/kp45microservicearchetype/SpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Kp45MicroserviceArchetypeApplication {
+public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Kp45MicroserviceArchetypeApplication.class, args);
+		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
 }
--- src/test/java/tech/kp45/kp45microservicearchetype/SpringbootApplicationTests.java
+++ src/test/java/tech/kp45/kp45microservicearchetype/SpringbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Kp45MicroserviceArchetypeApplicationTests {
+class SpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
